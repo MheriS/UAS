b2f66e47a87364993046bf71bcf7e5e683a0f5f1
Moh. Heri Susanto <79434378+MheriS@users.noreply.github.com>
2022-12-12 17:02:35 +0700

    Add files via upload

---
+++ lib/Autentikasi/login_header_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:uas/constant/size.dart';
+import 'package:uas/constant/image.dart';
+import 'package:uas/constant/text.dart';
+
+class LoginHeaderWidget extends StatelessWidget {
+  const LoginHeaderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(
+            image: const AssetImage(tWelcomeImage),
+            height: size.height * 0.2),
+        Text(tLoginTitle, style: TextStyle(
+          fontSize: 28,
+        )),
+        Text(tLoginSubTitle, style: Theme.of(context).textTheme.bodyText1),
+      ],
+    );
+  }
+}
---
+++ lib/constant/image.dart
@@ -0,0 +1,2 @@
+const String tWelcomeImage = "assets/images/violet.png";
+const String tGoogleLogoImage = "assets/images/google.png";
